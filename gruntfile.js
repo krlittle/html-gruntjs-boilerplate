76eb4d98721d88433894e2823a6aa2bfd34a8b53
Ken Little <krl49700@gmail.com>
2015-03-15 16:50:53 -0500

    integrate grunt watch plugin and minor file-folder structure updates

--- gruntfile.js
+++ gruntfile.js
@@ -1,26 +1,38 @@
 module.exports = function (grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
-        uglify: {
-            options: {
-                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
-            },
-            build: {
-                src: 'src/js/<%= pkg.name %>.js',
-                dest: 'build/js/<%= pkg.name %>.min.js'
-            }
-        },
         sass: {
-            dist: {
+            stylesheets: {
                 options: {
                     style: 'compressed'
                 },
                 files: { // 'destination': 'source'
-                    'build/css/<%= pkg.name %>.min.css': 'src/scss/<%= pkg.name %>.scss'
+                    'build/css/stylesheets.min.css': 'src/scss/*.scss'
                 }
             }
-        }
+        }, //sass
+        uglify: {
+            scripts: {
+                options: {
+                    banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+                },
+                files: { // 'destination': 'source'
+                    'build/js/scripts.min.js': 'src/js/*.js'
+                }
+            }
+        }, //uglify
+        watch: {
+            scripts: {
+                files: ['src/js/*.js'],
+                tasks: ['uglify']
+            }, //watch:scripts
+            stylesheets: {
+                files: ['src/scss/*.scss'],
+                tasks: ['sass']
+            } //watch:stylesheets
+        } //watch
     });
-    grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-watch');
 };
